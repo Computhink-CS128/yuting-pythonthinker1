b1fd5b7ad946cc72bded5f4dfcd17ab695e0e25a
Computhink-CS128 <cs128@Computhink.com.sg>
2025-01-19 03:58:29 +0000

    Jan 19, 2025, 3:58 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -5,3 +5,15 @@ print("Hello from lesson 2")
 
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+# 1. repeat 10 times
+#   2. print"hey"
+#   3. move 10 steps
+
+# 1. set "counter" to 0
+# 2. repeat until "counter"=50
+#    3. move 10 steps
+#    4. turn 15 degrees
+#    5. chage "counter" by 10
+
+if 
